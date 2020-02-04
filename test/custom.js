009eb2aff9eeda8ea59fa0c2fa3d1dd4999e69b5
Louis Brunet <123louisb@gmail.com>
2020-02-04 22:09:18 +0100

    vis eric

--- test/custom.js
+++ test/custom.js
@@ -12,36 +12,6 @@ var groups = new vis.DataSet([
       {id: 6, content: 'Traitements systémiques', value: 5}
     ]);
 
-// create a dataset with items
-// note that months are zero-based in the JavaScript Date object, so month 3 is April
-/*var items = new vis.DataSet([
-  {id: 0, group: 0, className: 'path-t', content: 'CHC', title: 'CHC', start: new Date(2014, 3, 17)},
-  {id: 1, group: 4, className: 'path-nt', content: 'Nash', title: 'Nash',  start: new Date(2014, 5, 17)},
-  {id: 2, group: 4, className: 'path-nt', content: 'Cir', title: 'Cir',  start: new Date(2013, 3, 25)},
-  {id: 3, group: 1, className: 'examen', content: 'IRM', start: new Date(2014, 4, 17)},
-  {id: 4, group: 1, className: 'examen', content: 'TDM', start: new Date(2018, 0, 17)},
-  {id: 5, group: 1, className: 'examen', content: 'Bio', start: new Date(2014, 7, 17)},
-  {id: 6, group: 1, className: 'examen', content: 'Cath', start: new Date(2016, 4, 10)},
-  {id: 7, group: 2, className: 'traitement', content: 'Res', title: 'Res | Traitement T',  start: new Date(2014, 4, 17)},
-  {id: 8, group: 2, className: 'traitement', content: 'MO', title: 'MO | Traitement NT',  start: new Date(2014, 6, 17)},
-  {id: 9, group: 2, className: 'traitement', content: 'RF', title: 'RF | Traitement NT',  start: new Date(2014, 7, 5)},
-  {id: 10, group: 2, className: 'traitement', content: 'RE', title: 'RE | Traitement NT',  start: new Date(2014, 6, 29)},
-  {id: 11, group: 2, className: 'traitement', content: 'TC-d', title: 'TC début | Traitement NT',  start: new Date(2014, 8, 23)},
-  {id: 12, group: 2, className: 'traitement', content: 'TC-f', title: 'TC fin | Traitement NT',  start: new Date(2014, 9, 23)},
-  {id: 13, group: 2, className: 'traitement', content: 'CEL', title: 'CEL | Traitement NT',  start: new Date(2014, 9, 10)},
-  {id: 14, group: 0, className: 'path-fin', content: 'CHC', title: 'Fin CHC', start: new Date(2014, 4, 25)},
-  {id: 15, group: 4, className: 'path-fin', content: 'Nash', title: 'Fin Nash', start: new Date(2014, 11, 25)},
-  {id: 16, group: 5, className: 'complications', content: 'Asc', title: 'Asc', start: new Date(2014, 9, 31)},
-  {id: 17, group: 4, className: 'path-fin', content: 'Hépat', title: 'Fin Hépat', start: new Date(2015, 7, 13)},
-  {id: 18, group: 4, className: 'path-nt', content: 'Hépat', title: 'Hépat',  start: new Date(2014, 9, 25)},
-  {id: 19, group: 0, className: 'path-t', content: 'CHC', title: 'CHC', start: new Date(2019, 0, 1)},
-  {id: 20, group: 3, className: 'consultation', content: 'RCP', title: 'RCP', start: new Date(2015, 7, 23)},
-  {id: 21, group: 3, className: 'consultation', content: 'Cs', title: 'Cs', start: new Date(2019, 0, 14)},
-  {id: 22, group: 3, className: 'consultation', content: 'Cs', title: 'Cs', start: new Date(2016, 4, 8)},
-  {id: 23, group: 3, className: 'consultation', content: 'Cs', title: 'Cs', start: new Date(2013, 3, 30)}
-]);*/
-
-
 // create visualization
 var container = document.getElementById('visualization');
 
@@ -132,6 +102,11 @@ request.onload = function () {
       parsedItem.title = parsedItem.content;
     }
 
+    if(parsedData[i].hasOwnProperty('link')) {
+      parsedItem.link = parsedData[i].link;
+    }
+
+
     loadedItems.push(parsedItem);
 
   }
@@ -142,6 +117,8 @@ request.onload = function () {
   // AFfichage
   timeline = new vis.Timeline(container, items, groups, options);
   hideAllEmptySpace(document.getElementById('tolerance').value);
+
+  timeline.on('select', onSelect);
 }
 
 
@@ -337,4 +314,11 @@ function incrementContents() {
       return false;
     }
   });
+}
+
+function onSelect(properties) {
+  let selectedItem = items.get(properties.items);
+  if(selectedItem.hasOwnProperty('link')){
+    window.open(selectedItem.link);
+  }
 }
