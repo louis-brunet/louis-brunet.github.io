bea450cde61e085559c05fbf0bff74d9dcb7c088
Louis Brunet <123louisb@gmail.com>
2020-02-08 13:34:16 +0100

    genes eric

--- test/genes/anomalies.js
+++ test/genes/anomalies.js
@@ -1,9 +1,34 @@
 /**
  * Création de la timeline
  */
-var aGroups = [];
-var aContainer = document.getElementById('structure');
-var aOptions = {};
+var aGroups = new vis.DataSet([
+	{id: 0, content: 'Mutations', value: 0},
+	{id: 1, content: 'Copy number', value: 1},
+	{id: 2, content: 'Expression', value: 2},
+	{id: 3, content: 'Mutilation', value: 3}
+]);
+var aContainer = document.getElementById('anomalies');
+var aOptions = {
+	groupOrder: function (a, b) {
+		return a.value - b.value;
+	  },
+	editable: false,
+	multiselect: false,
+	format: {
+		minorLabels: function(date, scale, step) {
+			return date.toDate().getTime();
+		}
+	},
+	orientation: {
+		axis: 'top'
+	},
+	timeAxis: {
+		scale: 'millisecond',
+		step: 10000
+	},
+	showMajorLabels: false,
+	stack: false
+};
 
 var aTimeline;
 var aItems = [];
@@ -13,7 +38,7 @@ aRequest.open('GET', aUrl);
 aRequest.responseType = 'json';
 aRequest.send();
 
-// sRequest.onload = createAnomaliesTimeline; 
+aRequest.onload = createAnomaliesTimeline; 
 
  /**
   * Fonctions
@@ -27,5 +52,52 @@ function createAnomaliesTimeline(){
 }
 
 function loadAnomaliesData(jsonData) {
+	let loadedItems = [];
 
+	if(Array.isArray(jsonData) ) {
+		for (let i = 0; i < jsonData.length; i++) {
+			loadAnomalie(jsonData[i], loadedItems);
+		}
+	}
+
+	sItems = new vis.DataSet(loadedItems);
+}
+
+loadAnomalie(parsedItem, itemArray) {
+	let item = {
+		id: itemArray.length,
+		group: null,
+		content: null,
+		start: parsedItem.start
+	};
+
+	switch(parsedItem.famille) {
+		case 'mutation':
+			item.group = 0;
+			break;
+		case 'copy':
+			item.group = 1;
+			break;
+		case'expr':
+			item.group = 2;
+			break
+		case'meth':
+			item.group = 3;
+			break
+	}
+
+	switch (parsedItem.type) {
+		
+	}
+
+	if(parsedItem.famille = 'mutation') {
+		switch(parsedItem.soustype) {
+			case 'somatic':
+				break;
+			case 'germline':
+				break;
+		}
+	}
+
+	itemArray.push(item);
 }
